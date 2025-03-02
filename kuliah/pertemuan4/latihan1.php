641f330e7dc8c39fc9a1764f576f634fe928395e
salwaalya <salwaalya008@gmail.com>
2025-03-02 10:25:52 +0700

    mengerjakantugas 1 dan 2

---
+++ kuliah/pertemuan4/latihan1.php
@@ -0,0 +1,20 @@
+<?php
+// PENGULANGAN / LOOP / LOOPING
+//while
+// komponen while :
+// 1. nilai awal / inisiallisasi
+// 2. kondisi terminasi / kapan harus berhenti
+// 3. increment / decrement
+
+$i = 5;
+while ($i >=1) {
+    echo "Hello World! $i kali <br>";
+    $i = $i-1;
+
+}
+
+echo "<hr>";
+
+for ($i = 1; $i <= 10; $i++ )  {
+    echo "Hello World! $i x <br>";
+}
---
+++ pertemuan2/latihan2.php
@@ -0,0 +1,11 @@
+<?php
+$nama = "Salwa Alya";
+$email = "salwaalya008@gmail.com";
+$pekerjaan = "Mahasiswa";
+$jurusan = "Teknik Informatika";
+$universitas = "Universitas Pasundan";
+echo "<h1>$nama</h1>";
+echo "<p>$email</p>";
+
+echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan di $jurusan $universitas";
+?>
